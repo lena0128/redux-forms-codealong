0d984aa23f7f2275bae5c7caff12afef5226c707
Lena Hou <lenahou2013@gmail.com>
2021-12-05 14:49:39 -0500

    commit tree b9c712971fc1abe21dbaafd921c2089be666f763

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class CreateTodo extends Component {
+
+  constructor(){
+      super()
+      this.state = {
+        text: ""
+      }
+  }
+
+  // arrow functions don't define their own version of `this` 
+  // they'll default to the context they are in.
+  // the value of `this` is now the specific instance of the CreateTodo component
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+
+  }
+
+  handleSubmit = (event) => {
+     event.preventDefault()
+     this.props.addTodo(this.state)
+  }
+
   render() {
     return(
-      <div>
-        Create Todo Component
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="add todo" name="name" onChange={this.handleChange} value={this.state.text} />
+        <input type="submit" />
+      </form>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (formData) => dispatch({type: "ADD_TODO", payload: formData}) 
+  } 
+}
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
